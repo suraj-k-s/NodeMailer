1c67860756c9030038c67812f55c367318fe2854
Suraj K S <56590063+suraj-k-s@users.noreply.github.com>
2023-04-06 14:48:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const mailer = require("nodemailer");
+var transporter = mailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "email@gmail.com", //from email Id
+    pass: "uttfqoevogxpsalg", // App password created from google account
+  },
+});
+function sendEmail(to, subject, content) {
+  const mailOptions = {
+    from: "email@gmail.com", //from email Id for recipient can view
+    to,
+    subject,
+    html: content,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sented");
+    }
+  });
+}
+
+const email = "surajks28101999@gmail.com"; //TO email id
+const sub = "demo"; //subject
+const con = "Hello World"; // content
+
+sendEmail(email, sub, con); //call this function for send email
